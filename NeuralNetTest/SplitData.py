e92e641c8aa146d9ddb1b37a31d85edb004ac8ca
Mohamed Ali Elakhrass <mohamedakhrass11@gmail.com>
2021-04-01 19:30:27 -0400

    Split data

---
+++ NeuralNetTest/SplitData.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+if __name__ == '__main__':
+    num_of_positions = 1500000
+    num_of_positions_per_file = 100000
+    num_of_train_positions = int(num_of_positions-100000)
+    whites = np.zeros((num_of_positions, 773), dtype='float32')
+    blacks = np.zeros((num_of_positions, 773), dtype='float32')
+    for i in range(15):
+        print(i + 1)
+        whites[i * 100000:(i + 1) * 100000] = np.load('./data_gen/white' + str(i + 1) + '.npy')
+        blacks[i * 100000:(i + 1) * 100000] = np.load('./data_gen/black' + str(i + 1) + '.npy')
+
+    print("Shuffling white positions")
+    np.random.shuffle(whites)
+    print("Shuffling black positions")
+    np.random.shuffle(blacks)
+
+    train_whites = whites[:num_of_train_positions]
+    train_blacks = blacks[:num_of_train_positions]
+
+    val_whites = whites[num_of_train_positions:]
+    val_blacks = blacks[num_of_train_positions:]
+
+    for i in range(int(num_of_train_positions/num_of_positions_per_file)):
+        np.save('./data5/white_train' + str(i+1) + '.npy', train_whites[i*num_of_positions_per_file:(i+1)*num_of_positions_per_file])
+        np.save('./data5/black_train' + str(i+1) + '.npy', train_blacks[i*num_of_positions_per_file:(i+1)*num_of_positions_per_file])
+        if i < int((num_of_positions-num_of_train_positions)/num_of_positions_per_file):
+            np.save('./data5/white_val' + str(i + 1) + '.npy', val_whites[i * num_of_positions_per_file:(i + 1) * num_of_positions_per_file])
+            np.save('./data5/black_val' + str(i + 1) + '.npy', val_blacks[i * num_of_positions_per_file:(i + 1) * num_of_positions_per_file])
