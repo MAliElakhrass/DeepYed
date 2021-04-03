c42852d035ec18c76a2af1bc6979d4c92f207402
Mohamed Ali Elakhrass <mohamedakhrass11@gmail.com>
2021-04-03 13:32:35 -0400

    Experiment

--- NeuralNetTest/Experiment.py
+++ NeuralNetTest/Experiment.py
@@ -7,7 +7,6 @@ import chess
 import chess.pgn
 import chess.polyglot
 import numpy as np
-import random
 
 
 class Experiment:
@@ -31,11 +30,7 @@ class Experiment:
         while not board.is_game_over():
             if board.turn:
                 print("DeepYed's Turn")
-                try:
-                    move = chess.polyglot.MemoryMappedReader('./books/Perfect2017-SF12.bin').weighted_choice(
-                        board=board).move
-                except:
-                    _, move = self.alpha_beta_max(board, None, None, None, None, 3)
+                _, move = self.alphabeta(board, 3, -100, 100, True, board)
                 history.append(move)
                 board.push(move)
                 print(move)
@@ -87,122 +82,54 @@ class Experiment:
         return self.deepyed([bitboard_1, bitboard_2])
 
     def compare_moves(self, move_1, move_2):
-        # bitboard_1, bitboard_2 = self.get_bitboard(move_1), self.get_bitboard(move_2)
-        value = self.apply_siamese_network(move_1, move_2)
-        return value[0]
+        bitboard_1, bitboard_2 = self.get_bitboard(move_1), self.get_bitboard(move_2)
+        return self.apply_siamese_network(bitboard_1, bitboard_2)[0][0]
 
-    def alpha_beta_max(self, board, alpha_pos, alpha_move, beta_pos, beta_move, depth):
-        if depth == 0 or board.is_game_over():
-            return self.get_bitboard(board)
-
-        # Copy the board
-        child_board = board.copy()
-
-        # Randomize legal moves
-        legal_moves = random.sample(list(board.legal_moves), len(list(board.legal_moves)))
-
-        # For every legal move,
-        for move in legal_moves:
-
-            # Make the move on the copy
-            child_board.push(move)
-
-            # Get the result of the move of the min player
-            pos, _ = self.alpha_beta_min(child_board, alpha_pos, alpha_move, beta_pos, beta_move, depth - 1)
-
-            # If the resulting position is better than the beta position, return the
-            # beta position which is the least best position for the max player that
-            # the min player is assured of. This represents fail-hard beta-cutoff.
-            # Having a guaranteed worse outcome for the max player, the min player
-            # won't enter this node since the max player has at least one move in
-            # this node with an outcome that dominates beta position. Note: None
-            # corresponds to an infinitely good outcome for the max player when
-            # assigned to the beta position.
-            if beta_pos is not None:
-                comparison = np.squeeze(self.compare_moves(pos, beta_pos))
-                if comparison[0] >= comparison[1]:
-                    return beta_pos, beta_move
-
-            # If the resulting position is better than the alpha position, assign it to
-            # to the alpha position which is the least worst position for the max player
-            # that the max player is assured of. After entering this node, the max player
-            # can make at least this move that results in a better position than the alpha
-            # position. Hence, the resulting position is the new guaranteed least worst case.
-            # Note: None corresponds to an infinitely bad outcome for the max player when
-            # assigned to the alpha position.
-            if alpha_pos is None:
-                alpha_pos = pos
-                alpha_move = move
+    def alphabeta(self, board, depth, alpha, beta, white, orig_board):
+        try:
+            move = chess.polyglot.MemoryMappedReader('./books/Perfect2017-SF12.bin').weighted_choice(
+                board=orig_board).move
+            return None, move
+        except:
+            if depth == 0:
+                yed = self.compare_moves(board, orig_board)
+                return yed, None
+            if white:
+                v = -100  # very (relatively) small number
+                moves = board.generate_legal_moves()
+                moves = list(moves)
+                best_move = None
+                for move in moves:
+                    new_board = board.copy()
+                    new_board.push(move)
+                    candidate_v, _ = self.alphabeta(new_board, depth - 1, alpha, beta, True, orig_board)
+                    if candidate_v >= v:
+                        v = candidate_v
+                        best_move = move
+                    else:
+                        pass
+                    alpha = max(alpha, v)
+                    if beta <= alpha:
+                        break
+                return v, best_move
             else:
-                comparison = np.squeeze(self.compare_moves(pos, alpha_pos))  # self.deep_chess.evaluate(self.sess, pos, alpha_pos))
-                if comparison[0] > comparison[1]:
-                    alpha_pos = pos
-                    alpha_move = move
-
-            # Unmake the move
-            child_board = board.copy()
-
-        # Return the alpha position (and the corresponding move) as
-        # it is the guaranteed worst-case scenario for the max player.
-        return alpha_pos, alpha_move
-
-    def alpha_beta_min(self, board, alpha_pos, alpha_move, beta_pos, beta_move, depth):
-
-        # Return the normalized bitboard representation when
-        # the node is a final one or the depth limit is reached
-        if depth == 0 or board.is_game_over():
-            return self.get_bitboard(board), None
-
-        # Copy the board
-        child_board = board.copy()
-
-        # Randomize legal moves
-        legal_moves = random.sample(list(board.legal_moves), len(list(board.legal_moves)))
-
-        # For every legal move,
-        for move in legal_moves:
-
-            # Make the move on the copy
-            child_board.push(move)
-
-            # Get the result of the move of the max player
-            pos, _ = self.alpha_beta_max(child_board, alpha_pos, alpha_move, beta_pos, beta_move, depth - 1)
-
-            # If the resulting position is worse than the alpha position, return the
-            # alpha position which is the least worst position for the max player that
-            # the max player is assured of. This represents fail-hard alpha-cutoff.
-            # Having a guaranteed better outcome for the itself, the max player
-            # won't enter this node since the min player has at least one move in
-            # this node with an outcome that is worse than the alpha position.
-            # Note: None corresponds to an infinitely bad outcome for the max player
-            # when assigned to the alpha position.
-            if alpha_pos is not None:
-                comparison = np.squeeze(self.compare_moves(pos, alpha_pos))
-                if comparison[0] <= comparison[1]:
-                    return alpha_pos, alpha_move
-
-            # If the resulting position is worse than the beta position, assign it to
-            # to the beta position which is the least best position for the max player
-            # that the min player is assured of. After entering this node, the min player
-            # can make at least this move that results in a worse position for the max player
-            # than the beta position. Hence, the resulting position is the new guaranteed
-            # least worst case. Note: None corresponds to an infinitely good outcome for
-            # the max player when assigned to the beta position.
-            if beta_pos is None:
-                beta_pos = pos
-                beta_move = move
-            else:
-                comparison = np.squeeze(self.compare_moves(pos, beta_pos))
-                if comparison[0] < comparison[1]:
-                    beta_pos = pos
-                    beta_move = move
-
-            # Unmake the move
-            child_board = board.copy()
-
-        # Return the beta position (and the corresponding move) as
-        # it is the guaranteed worst-case scenario for the min player.
-        return beta_pos, beta_move
+                v = 100  # very (relatively) large number
+                moves = board.generate_legal_moves()
+                moves = list(moves)
+                best_move = None
+                for move in moves:
+                    new_board = board.copy()
+                    new_board.push(move)
+                    candidate_v, _ = self.alphabeta(new_board, depth - 1, alpha, beta, False, orig_board)
+                    if candidate_v <= v:
+                        v = candidate_v
+                        best_move = move
+                    else:
+                        pass
+                    beta = min(beta, v)
+                    if beta <= alpha:
+                        break
+                return v, best_move
 
     def get_move_stockfish(self, board, stockfish):
         fen = board.fen()
