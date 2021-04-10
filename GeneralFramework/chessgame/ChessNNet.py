ddb39ecf606820f939cc50cb3d40d0c1a9316d1e
Mohamed Ali Elakhrass <mohamedakhrass11@gmail.com>
2021-04-10 18:04:12 -0400

    Fix small bugs

--- GeneralFramework/chessgame/ChessNNet.py
+++ GeneralFramework/chessgame/ChessNNet.py
@@ -16,14 +16,13 @@ class ChessNNet:
 
     def construct_graph(self):
         input_boards = Input(name='input', shape=(self.board_x, self.board_y))
-        dropout = Input(name='dropout')
 
         x_image = Reshape((self.board_x, self.board_y, 1))(input_boards)
-        h_conv1 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(self.args['num_channels'], padding='same', use_bias=False)(x_image)))
-        h_conv2 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(self.args['num_channels'], padding='same', use_bias=False)(h_conv1)))
-        h_conv3 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(self.args['num_channels'], padding='same', use_bias=False)(h_conv2)))
-        h_conv4 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(self.args['num_channels'], padding='same', use_bias=False)(h_conv3)))
-        h_conv5 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(self.args['num_channels'], padding='same', use_bias=False)(h_conv4)))
+        h_conv1 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(self.args['num_channels'], 3, padding='same', use_bias=False)(x_image)))
+        h_conv2 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(self.args['num_channels'], 3, padding='same', use_bias=False)(h_conv1)))
+        h_conv3 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(self.args['num_channels'], 3, padding='same', use_bias=False)(h_conv2)))
+        h_conv4 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(self.args['num_channels'], 3, padding='same', use_bias=False)(h_conv3)))
+        h_conv5 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(self.args['num_channels'], 3, padding='same', use_bias=False)(h_conv4)))
         h_conv5_flat = Flatten()(h_conv5)
 
         s_fc1 = Dropout(self.args['dropout'])(Activation('relu')(BatchNormalization(axis=1)(Dense(1024, use_bias=False)(h_conv5_flat))))
@@ -35,7 +34,7 @@ class ChessNNet:
         pi = Dense(self.action_size, activation='softmax', name='pi')(s_fc2)
         v = Dense(1, activation='tanh', name='v')(s_fc2)
 
-        model = Model(inputs=self.input_boards, outputs=[self.pi, self.v])
+        model = Model(inputs=input_boards, outputs=[pi, v])
         model.compile(loss=['categorical_crossentropy', 'mean_squared_error'], optimizer=Adam(self.args['lr']))
 
         return model
